26e36752f47905b5108414379c28bdb2ad71db72
Mac7ota <mauro.alves.aguiar@outlook.com>
2022-09-17 02:10:38 -0300

    Terminando

---
+++ JavaScript/quests.js
@@ -0,0 +1,13 @@
+let jogador = prompt('Qual o seu nome jogador:')
+alert('Muito bem vindo ' + jogador)
+
+let pronto = prompt('Está pronto para mais uma aventura! sim/não')
+
+if (pronto === 'sim'){
+    alert("Pegue seus equipamentos " + jogador + "\nUma nova aventura irá começar!")
+    alert("Por te sido o primeiro a achar o portal para essa missão 'RECEBA' \nUm cajado capaz de controlar o fogo \nUm escudo capaz de anular o fogo \nE uma armadura completa de aço vridano capaz de aguentar grandes temperaturas")
+    alert("Agora pegue a pipoca e se prepare pois o Cataclysma esta de volta as terras de Arkan \nBom video!")
+
+} else {
+    alert("Uma pena você não estar pronto mas quem sabe na proxima esteja disposto a encarar essa missão!")
+}
